52f54dfa940278c79d4391bb6297246a693eca28
KitagawaFuuga <c1104420@planet.kanazawa-it.ac.jp>
2024-07-08 20:58:21 +0900

    poi

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      コンちくわ
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>へろー</h1>
+      </header>
     </div>
   );
 }
